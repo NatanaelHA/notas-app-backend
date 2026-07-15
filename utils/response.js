11efa158c6c14c8f323f4597f48d9e698f129e2f
natanael-huenullan <natanaelhuenullan31@gmail.com>
2026-07-15 13:42:20 -0400

    fix: actualizar headers CORS en response utilsv2

--- utils/response.js
+++ utils/response.js
@@ -1,19 +1,21 @@
-const response = (statusCode, body) => {
+const response = (statusCode, body, event = null) => {
+  let origin = "*";
+  if (event && event.headers) {
+    origin = event.headers.origin || event.headers.Origin || "*";
+  }
+
   return {
-    statusCode,
+    statusCode: statusCode,
     headers: {
-      'Content-Type': 'application/json',
-      // ( para localhost y producción)
-      'Access-Control-Allow-Origin': '*',
-      
-      //
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token',
-      
-      //
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+      "Content-Type": "application/json",
+      // Usamos comillas dobles estrictas para evitar errores de parseo en la infraestructura
+      "Access-Control-Allow-Origin": origin,
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Credentials": "true"
     },
     body: JSON.stringify(body)
-  }
-}
+  };
+};
 
-module.exports = { response }
+module.exports = { response };
