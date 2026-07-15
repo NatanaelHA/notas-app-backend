23bc0db104ead8a92d1c7b9388725cb495cae7f1
natanael-huenullan <natanaelhuenullan31@gmail.com>
2026-07-15 12:59:34 -0400

    fix: actualizar headers CORS en response utils

--- utils/response.js
+++ utils/response.js
@@ -3,7 +3,14 @@ const response = (statusCode, body) => {
     statusCode,
     headers: {
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
+      // ( para localhost y producción)
+      'Access-Control-Allow-Origin': '*',
+      
+      //
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token',
+      
+      //
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
     },
     body: JSON.stringify(body)
   }
