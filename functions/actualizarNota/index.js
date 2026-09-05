6821dfb6d6aa336b5b6e9524baf2d0cfe85a56cb
natanael-huenullan <natanaelhuenullan31@gmail.com>
2026-09-05 14:05:48 -0400

    FEAT: agregar función para asociar adjunto a nota

--- functions/actualizarNota/index.js
+++ functions/actualizarNota/index.js
@@ -5,8 +5,8 @@ exports.handler = async (event) => {
   try {
     const userId = event.requestContext.authorizer.jwt.claims.sub
     const { noteId } = event.pathParameters
-    const { titulo, cuerpo, adjuntoKey } = JSON.parse(event.body)
-    const nota = await actualizarNota(userId, noteId, titulo, cuerpo, adjuntoKey)
+    const { titulo, cuerpo } = JSON.parse(event.body)
+    const nota = await actualizarNota(userId, noteId, titulo, cuerpo)
     return response(200, { data: nota })
   } catch (error) {
     console.error('Error al actualizar nota:', error)
